afe0fcf8724b725de559adaa54ab4196d45e228a
Steve Godin <steve.god@gmail.com>
2018-07-25 10:33:04 -0700

    Add demo for Dynamic Question Details

--- src/index.js
+++ src/index.js
@@ -5,18 +5,27 @@ import registerServiceWorker from "./registerServiceWorker";
 
 // A function that returns a React Element (the value returned
 // by a call from React.createElement(...)) is a React Component.
-const QuestionDetails = () => {
+const QuestionDetails = props => {
   return (
     <div>
-      <h2>What is your favourite colour?</h2>
-      <p>Red, green, blue, magenta, yellow, cyan, etc.</p>
-      <p>By Bridge Troll</p>
+      <h2>{props.title}</h2>
+      <p>{props.body}</p>
+      <p>By {props.author.full_name}</p>
       <p>
-        <small>Seen 10 time(s)</small>
+        <small>
+          <strong>View Count: </strong>
+          {props.view_count}
+        </small>
         {" • "}
-        <small>Created 10 days ago</small>
+        <small>
+          <strong>Created At: </strong>
+          {props.created_at.toLocaleString()}
+        </small>
         {" • "}
-        <small>Last edited 2 hours ago</small>
+        <small>
+          <strong>Updated At: </strong>
+          {props.updated_at.toLocaleString()}
+        </small>
       </p>
     </div>
   );
@@ -36,5 +45,21 @@ const AnswerDetails = () => {
   );
 };
 
-ReactDOM.render(<AnswerDetails />, document.getElementById("root"));
+const QuestionShowPage = () => {
+  return (
+    <main>
+      <QuestionDetails
+        title="What is your favourite colour?"
+        body="Red, green, blue, light golden rod, lime, magenta, etc."
+        author={{ full_name: "Bridge Troll" }}
+        view_count={22}
+        created_at={new Date()}
+        updated_at={new Date()}
+      />
+      <AnswerDetails />
+    </main>
+  );
+};
+
+ReactDOM.render(<QuestionShowPage />, document.getElementById("root"));
 registerServiceWorker();
