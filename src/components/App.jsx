5f4d87c2018bf19a4512eda6021ee4dc197ffcbf
Steve Godin <steve.god@gmail.com>
2018-08-01 13:46:22 -0700

    Add demo for Sign In Page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ import QuestionIndexPage from "./QuestionIndexPage";
 import QuestionShowPage from "./QuestionShowPage";
 import QuestionNewPage from "./QuestionNewPage";
 import WelcomePage from "./WelcomePage";
+import SignInPage from "./SignInPage";
 
 const App = props => {
   return (
@@ -19,6 +20,7 @@ const App = props => {
           <Route path="/questions" exact component={QuestionIndexPage} />
           <Route path="/questions/new" exact component={QuestionNewPage} />
           <Route path="/questions/:id" component={QuestionShowPage} />
+          <Route path="/sign_in" component={SignInPage} />
         </Switch>
       </div>
     </Router>
