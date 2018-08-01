f961ae4c1a533d464ef6b4a25333c84d75c74533
Steve Godin <steve.god@gmail.com>
2018-08-01 15:27:27 -0700

    Add demo for Sign Out Button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 // We can rename (or alias) named imports
 // by using the `as` keyword as shown below:
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -9,22 +9,75 @@ import QuestionShowPage from "./QuestionShowPage";
 import QuestionNewPage from "./QuestionNewPage";
 import WelcomePage from "./WelcomePage";
 import SignInPage from "./SignInPage";
+import User from "../requests/user";
+import Session from "../requests/session";
 
-const App = props => {
-  return (
-    <Router>
-      <div>
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={WelcomePage} />
-          <Route path="/questions" exact component={QuestionIndexPage} />
-          <Route path="/questions/new" exact component={QuestionNewPage} />
-          <Route path="/questions/:id" component={QuestionShowPage} />
-          <Route path="/sign_in" component={SignInPage} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      currentUser: undefined
+    };
+
+    this.getUser = this.getUser.bind(this);
+    this.destroySession = this.destroySession.bind(this);
+  }
+
+  destroySession() {
+    Session.destroy().then(() => {
+      this.setState({ currentUser: undefined });
+    });
+  }
+
+  getUser() {
+    return User.current().then(data => {
+      if (data.id) {
+        this.setState({
+          currentUser: data
+        });
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getUser().then(() => {
+      this.setState({ loading: false });
+    });
+  }
+
+  render() {
+    const { currentUser, loading } = this.state;
+
+    if (loading) {
+      return (
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      );
+    }
+
+    return (
+      <Router>
+        <div>
+          <NavBar onSignOut={this.destroySession} currentUser={currentUser} />
+          <Switch>
+            <Route path="/" exact component={WelcomePage} />
+            <Route path="/questions" exact component={QuestionIndexPage} />
+            <Route path="/questions/new" exact component={QuestionNewPage} />
+            <Route path="/questions/:id" component={QuestionShowPage} />
+            <Route
+              path="/sign_in"
+              render={props => (
+                <SignInPage {...props} onSignIn={this.getUser} />
+              )}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,21 +2,39 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = props => {
+  const { currentUser, onSignOut = () => {} } = props;
+
+  const handleClick = event => {
+    event.preventDefault();
+    onSignOut();
+  };
+
   return (
     <nav className="NavBar">
       {/* <a href="/">Welcome</a> */}
       <NavLink exact to="/">
         Welcome
       </NavLink>
-      <NavLink exact to="/questions/new">
-        New Question
-      </NavLink>
+      {currentUser && (
+        <NavLink exact to="/questions/new">
+          New Question
+        </NavLink>
+      )}
       <NavLink exact to="/questions">
         Questions
       </NavLink>
-      <NavLink className="supports-classname" exact to="/sign_in">
-        Sign In
-      </NavLink>
+      {currentUser ? (
+        <React.Fragment>
+          <span>{currentUser.full_name}</span>
+          <a onClick={handleClick} href="#not-used">
+            Sign Out
+          </a>
+        </React.Fragment>
+      ) : (
+        <NavLink className="supports-classname" exact to="/sign_in">
+          Sign In
+        </NavLink>
+      )}
     </nav>
   );
 };
